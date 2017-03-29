958f9cb31481d0efc5f125c1b5d43b77d37d0029
mcarlson13-437505 <michaelcarlson13@augustana.edu>
2017-03-28 21:52:19 -0500

    Add files via upload

--- BSFCgui.java
+++ BSFCgui.java
@@ -21,42 +21,10 @@ public class BSFCgui implements ActionListener {
 	private JButton calcButton = new JButton("Calculate");
 	private JButton clearButton = new JButton("Clear Fields");
 
-	private double[] bsfcArray = { 1932.476445, 1594.992209, 660.9056872, 1521.261226, 1482.622717, 1543.870664,
-			538.5060152, 516.0939077, 1173.874463, 188.0786942, 154.7998661, 138.4100983, 261.4457486, 291.1977361,
-			208.505942, 314.7035908, 293.0854415, 70.22112935, 285.1594852, 72.78887319, 255.3589648, 921.121357,
-			264.4429473, 273.173962, 342.3346304, 244.150086, 1177.034733, 266.6490523, 440.5513508, 1314.972644,
-			838.2107654, 678.5271689, 120.7800437, 79.29803116, 279.710012, 238.4147337, 333.7062057, 153.7905558,
-			309.7035376, 297.8233957, 316.6775381, 346.0542332, 285.9808226, 291.1730044, 310.8419848, 396.9953136,
-			144.1454501, 291.9693866, 319.1305323, 383.0558685, 284.5858242, 287.940403, 3.507899774, 314.4626775,
-			234.4460514, 278.2402068, 402.263044, 298.8411414, 527.0361828, 369.2176705, 305.4711334, 309.9572376,
-			332.9214856, 396.6373913, 341.9714598, 348.6949409, 314.6871073, 314.2601234, 309.7818193, 314.5635358,
-			303.2279129, 291.5523331, 322.5586923, 315.751814, 234.9573586, 2558.746305, 418.0431635, 510.6226268,
-			1972.57751, 367.8577462, 200.4144411, 361.953447, 266.3824909, 311.5349385, 2.787823037, 2.900359013,
-			2.723902342, 170.087446, 199.9606165, 159.2726373, 209.1960286, 313.0357164, 337.1038441, 376.8120932,
-			127.9603326, 465.7022618, 109.1282783, 290.2838658, 261.0353289, 637.3135453, 275.9860469, 298.7523944,
-			0.6969557593, 288.1417599, 360.0490781, 288.4549088, 286.6708485, 312.9681772, 981.1239144, 3.905775914,
-			133.0333098, 431.9863351, 578.5478921, 282.8235391, 155.3576403, 305.4363554, 781.8611823, 456.4035018,
-			235.0504977, 489.1955478, 445.1933793, 339.4200166, 343.806749, 3.380571601, 3.319653893, 236.6540562,
-			357.2653112, 480.2915996, 419.5984822, 241.9421334, 422.3337333, 278.7559509, 412.1860613, 348.7808646,
-			84.49258751, 266.2313493, 329.6000079, 329.4281517, 4.232277366, 329.205822, 478.6854764, 463.7623852,
-			316.2166033, 307.7263777, 179.5948322, 744.5678257 }; // g/kwHr
+	int w1 = 1500, w2 = 1504, w3 = 1635, w4 = 1703, w5 = 1980, w6 = 2079, w7 = 2145, w8 = 2449, w9 = 2762;
+	double t1 = 33.35312157, t2 = 28.90603869, t3 = 26.68249726, t4 = 13.34124863, t5 = 31.12958013, t6 = 31.12958013,
+			t7 = 40.02374588, t8 = 15.56479007, t9 = 36.68843373;
 
-	private double[] speedArray = { 1, 1, 1, 1, 1, 1, 1, 1, 2, 6, 7, 8, 10, 11, 11, 13, 17, 17, 18, 18, 19, 19, 20, 20,
-			20, 20, 23, 25, 25, 27, 27, 27, 27, 27, 29, 29, 29, 29, 30, 30, 30, 31, 31, 31, 32, 32, 32, 32, 32, 32, 32,
-			33, 33, 33, 33, 33, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 34, 35, 35, 35,
-			35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35, 35,
-			36, 36, 36, 37, 37, 37, 37, 37, 39, 39, 39, 40, 40, 41, 42, 42, 42, 43, 43, 43, 43, 44, 45, 45, 45, 45, 47,
-			47, 47, 47, 48, 50, 50, 51, 51, 53, 53, 53, 53, 53, 58 }; // MPH
-
-	private int[] rpmArray = { 673, 755, 1757, 737, 731, 702, 694, 700, 977, 1830, 1496, 1300, 1890, 2001, 2431, 2246,
-			995, 2061, 965, 1760, 1008, 1752, 1184, 2061, 2076, 2507, 1512, 1143, 1487, 1669, 1639, 1886, 1724, 1692,
-			1372, 1121, 819, 1737, 1193, 1465, 1725, 1312, 1512, 1478, 1527, 1500, 1439, 1545, 1500, 2502, 1594, 1674,
-			1610, 1576, 1980, 1563, 1658, 1600, 1455, 1608, 1639, 1584, 1687, 1416, 1597, 1514, 1591, 1591, 1614, 1605,
-			1665, 1640, 2176, 1555, 1972, 2059, 1668, 2783, 1761, 1386, 2033, 1109, 1229, 1639, 1701, 1635, 1607, 1475,
-			1708, 1475, 1123, 1654, 1660, 1654, 1705, 1656, 1714, 1660, 1704, 1984, 1666, 1656, 1701, 1625, 1867, 1130,
-			2101, 1700, 1703, 1812, 1838, 2176, 2134, 1836, 1438, 1948, 1936, 1902, 1933, 1970, 1961, 2076, 2079, 2433,
-			2106, 2031, 2105, 2145, 2149, 2117, 2090, 2270, 2197, 2265, 2275, 2334, 2389, 2355, 2449, 2500, 2530, 2519,
-			2509, 2530, 2550, 2762 }; // RPM
 	private int[] r30 = { 1193, 1465, 1725 }, r31 = { 1312, 1512, 1478 },
 			r32 = { 1527, 1500, 1439, 1545, 1500, 2502, 1594 }, r33 = { 1674, 1610, 1576, 1980, 1563 },
 			r34 = { 1658, 1600, 1455, 1608, 1639, 1584, 1687, 1416, 1597, 1514, 1591, 1591, 1614, 1605, 1665, 1640,
@@ -110,7 +78,9 @@ public class BSFCgui implements ActionListener {
 			b50 = { 329.6000079, 329.4281517 }, b51 = { 4.232277366, 329.205822 },
 			b53 = { 478.6854764, 463.7623852, 316.2166033, 307.7263777, 179.5948322 }, b58 = { 744.5678257 };
 
+	@SuppressWarnings("rawtypes")
 	private JComboBox torqueDropDown;
+	@SuppressWarnings("unused")
 	private String accelType;
 	private JLabel torqueLabel;
 
@@ -145,46 +115,18 @@ public class BSFCgui implements ActionListener {
 		double lower = initialSpeed - (initialSpeed * .05);
 		System.out.println("Upper five %: " + upper);
 		System.out.println("Lower five %: " + lower);
-		int index = 0;
-		int sentinel = 0;
-		while (sentinel != -1) {
-			if (index == speedArray.length) {
-				upper = initialSpeed + (initialSpeed * .1);
-				lower = initialSpeed - (initialSpeed * .1);
-				System.out.println("Upper Ten %: " + upper);
-				System.out.println("Lower Ten %: " + lower);
-				valueGrabber();
-			} else if (speedArray[index] == initialSpeed
-					|| (speedArray[index] <= upper && speedArray[index] >= lower)) {
-				sentinel = -1;
-			} else {
-				index++;
-			}
-		}
-		System.out.println("Index: " + index);
 		System.out.println("Initial Speed: " + initialSpeed);
 		System.out.println("Final Speed: " + finalSpeed);
-		int rpm = rpmArray[index];
 		int rpmToUse = grabRpm(initialSpeed);
+		System.out.println("RPM: " + rpmToUse);
 		// finds the average of torques within that array for calculations
 		double torqueToUse = grabTorque(initialSpeed);
 		System.out.println("Torque: " + torqueToUse);
-		double bsfc = bsfcArray[index];
 		double bsfcToUse = grabBsfc(initialSpeed);
-		System.out.println("BSFC: " + bsfcToUse);
-		double distance = 0;
-		double massConsumed = calculateMass(initialSpeed, distance, rpm, bsfc, torqueToUse);
-		double volumeConsumed = calculateVolume(massConsumed);
-		double mpg = calculateMPG(massConsumed, distance);
-		System.out.println("RPM: " + rpmToUse);
-		System.out.println("Mass consumed: " + massConsumed);
-		System.out.println("Volume consumed: " + volumeConsumed);
-		System.out.println("MPG: " + mpg);
-		massField.setValue(massConsumed);
-		volumeField.setValue(volumeConsumed);
-		mpgField.setValue(mpg);
-		// speed = speed * 0.44704; // mph to meter/s
-		// velocityLoop(distance, rpm, torque, bsfc, speed);
+		System.out.println("BSFC: " + bsfcToUse + "\n");
+		initialSpeed = initialSpeed * 0.44704; // mph to meter/sec
+		finalSpeed = finalSpeed * 0.44704; // mph to meter/sec
+		velocityLoop(rpmToUse, torqueToUse, bsfcToUse, initialSpeed, finalSpeed);
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -719,8 +661,6 @@ public class BSFCgui implements ActionListener {
 	 * calculates mpg for the trip
 	 */
 	private double calculateMPG(double massConsumed, double distance) {
-		// TODO: figure out way to get distance from integral based on time and
-		// speed
 		double mpg = 0;
 		double poundMass = massConsumed * 2.20462; // kg to lb
 		double volume = poundMass / 6.94; // in gallons, 6.94 = density diesel
@@ -733,7 +673,6 @@ public class BSFCgui implements ActionListener {
 	 * calculates mass consumed and returns the value as a long
 	 */
 	private double calculateMass(double speed, double distance, double rpm, double bsfc, double torque) {
-		// TODO: pass distance from mpg method
 		double mass = 0;
 		double bsfc1 = bsfc / 3600000; // converts to g/s
 		double rpm1 = rpm * (2 * Math.PI / 60);
@@ -744,44 +683,109 @@ public class BSFCgui implements ActionListener {
 		return mass;
 	}
 
-	// /*
-	// * loop to calculate mass and velocities
-	// */
-	// private void velocityLoop(double distance, double w, double T, double
-	// BSFC, double velocityVehicle, double initialSpeed, double finalSpeed) {
-	// double wcruise = 0; // get these from csv
-	// double Tcruise = 0; // get these from csv
-	// double massVehicle = 1060.045; // (kg)
-	// double velocityOld = 30 * 0.44704; // accelerating from 30mph
-	// double Dt = .1;
-	// double Dvelocity;
-	// double velocityNew;
-	// double dist = 0;
-	//
-	// System.out.println("Dist: " + dist);
-	// System.out.println("Velocity old: " + velocityOld);
-	// if (velocityVehicle > 20 && velocityVehicle < 125) {
-	// while (dist < distance) {
-	// Dvelocity = (w * T - (wcruise * Tcruise)) / (massVehicle *
-	// velocityVehicle);
-	// System.out.println("Velocity old: " + velocityOld);
-	// velocityNew = velocityOld + Dvelocity;
-	// velocityOld = velocityNew;
-	// System.out.println("D velocity: " + Dvelocity);
-	// System.out.println("Velocity new: " + velocityNew);
-	// dist = dist + velocityVehicle * Dt; // = SUM(velocity*Dt);
-	// System.out.println("dist: " + dist + "\n");
-	// w++;
-	// T++;
-	// }
-	// } else {
-	// System.out.println("Speed too low or too high.");
-	// }
-	// }
+	/*
+	 * loop to calculate mass and velocities
+	 */
+	private void velocityLoop(int rpm, double torque, double bsfc, double initialSpeed,
+			double finalSpeed) {
+		int wcruise = getCruiseRpm(initialSpeed, finalSpeed);
+		double Tcruise = getCruiseTorque(initialSpeed, finalSpeed);
+		double massVehicle = 1060.045; // (kg)
+		double velocityOld = initialSpeed;
+		double Dt = .1;
+		double sumTime = 0;
+		double Dvelocity = 0;
+		double velocityNew = velocityOld + Dvelocity;
+		double dist = 0;
+		double mass = 0;
+		double Dmass;
+
+		System.out.println("Dist: " + dist);
+		System.out.println("Initial Speed: " + velocityOld);
+		//infinite loop!!!!!!!!!!!********HELP
+			while (velocityOld <= velocityNew) {
+				System.out.println("\n\n");
+				Dvelocity = (rpm * torque - (wcruise * Tcruise)) / (massVehicle * velocityOld);
+				System.out.println("Velocity old: " + velocityOld);
+				velocityNew = velocityOld + Dvelocity;
+				velocityOld = velocityNew;
+				System.out.println("D velocity: " + Dvelocity);
+				System.out.println("Velocity new: " + velocityNew);
+				dist = dist + velocityNew * Dt; // = SUM(velocity*Dt);
+				System.out.println("dist: " + dist);
+				sumTime = sumTime + Dt;
+				System.out.println("Sum time: " + sumTime);
+				Dmass = bsfc * rpm * torque * Dt;
+				System.out.println("Dmass: " + Dmass);
+				mass = mass + Dmass;
+				System.out.println("Mass: " + mass);
+			}
+		double massConsumed = calculateMass(velocityNew, dist, rpm, bsfc, torque);
+		double volumeConsumed = calculateVolume(massConsumed);
+		double mpg = calculateMPG(massConsumed, dist);
+		System.out.println("Mass consumed: " + massConsumed);
+		System.out.println("Volume consumed: " + volumeConsumed);
+		System.out.println("MPG: " + mpg);
+		massField.setValue(massConsumed);
+		volumeField.setValue(volumeConsumed);
+		mpgField.setValue(mpg);
+	}
 
 	/*
 	 * cruising torque solve y = .0212x where y = torque, x = rpm
 	 */
+
+	private double getCruiseTorque(double initialSpeed, double finalSpeed) {
+		double t = 0;
+		if (initialSpeed == 32 || finalSpeed == 32) {
+			t = t1;
+		} else if (initialSpeed == 34 || finalSpeed == 34) {
+			t = t2;
+		} else if (initialSpeed == 35 || finalSpeed == 35) {
+			t = t3;
+		} else if (initialSpeed == 43 || finalSpeed == 43) {
+			t = t5;
+		} else if (initialSpeed == 37 || finalSpeed == 37) {
+			t = t4;
+		} else if (initialSpeed == 45 || finalSpeed == 45) {
+			t = t6;
+		} else if (initialSpeed == 48 || finalSpeed == 48) {
+			t = t7;
+		} else if (initialSpeed == 51 || finalSpeed == 51) {
+			t = t8;
+		} else if (initialSpeed == 58 || finalSpeed == 58) {
+			t = t9;
+		} else {
+			t = 31.6;
+		}
+		return t;
+	}
+
+	private int getCruiseRpm(double initialSpeed, double finalSpeed) {
+		int w = 0;
+		if (initialSpeed == 32 || finalSpeed == 32) {
+			w = w1;
+		} else if (initialSpeed == 34 || finalSpeed == 34) {
+			w = w2;
+		} else if (initialSpeed == 35 || finalSpeed == 35) {
+			w = w3;
+		} else if (initialSpeed == 43 || finalSpeed == 43) {
+			w = w5;
+		} else if (initialSpeed == 37 || finalSpeed == 37) {
+			w = w4;
+		} else if (initialSpeed == 45 || finalSpeed == 45) {
+			w = w6;
+		} else if (initialSpeed == 48 || finalSpeed == 48) {
+			w = w7;
+		} else if (initialSpeed == 51 || finalSpeed == 51) {
+			w = w8;
+		} else if (initialSpeed == 58 || finalSpeed == 58) {
+			w = w9;
+		} else {
+			w = 1870;
+		}
+		return w;
+	}
 
 	public static void main(String[] args) {
 		@SuppressWarnings("unused")
